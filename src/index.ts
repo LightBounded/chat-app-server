f5ab6335f87b06b8cddd0e2b5677cfcebb0d0b16
Caleb Rivera <calebrossr@gmail.com>
2023-03-15 13:28:59 -0400

    Add log in

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
 app.use(express.json());
 app.use(cors());
-
-const server = createServer(app);
-const io = new Server(server);
 
 type Channel = {
   id: string;
@@ -26,6 +30,7 @@ type Message = {
 type User = {
   id: string;
   username: string;
+  isOnline: boolean;
 };
 
 let messages: Message[] = [];
@@ -33,14 +38,40 @@ let users: User[] = [];
 let channels: Channel[] = [];
 let onlineUserIds: string[] = [];
 
-function fetchAndEmitUsers() {}
-
 io.on("connection", (socket) => {
   console.log("a user has connected");
 
-  socket.on("message", (message) => {
-    messages.push(message);
-    io.emit("message", message);
+  socket.on("messageCreate", (message) => {
+    const newMessage = { ...message, id: randomUUID() };
+    messages.push(newMessage);
+    io.emit("messageCreate", newMessage);
+  });
+
+  socket.on("usersFetch", () => {
+    io.emit("usersFetch", users);
+  });
+
+  socket.on("userCreate", (username) => {
+    const newUser = { username, isOnline: false, id: randomUUID() };
+    users.push(newUser);
+    io.emit("userCreate", newUser);
+  });
+
+  socket.on("logIn", (userId) => {
+    onlineUserIds.push(userId);
+
+    const onlineUsers = users.map((u) => ({
+      ...u,
+      isOnline: onlineUserIds.includes(u.id),
+    }));
+
+    io.emit("usersFetch", onlineUsers);
+  });
+
+  socket.on("channelCreate", (channelName: string) => {
+    const newChannel = { name: channelName, id: randomUUID() };
+    channels.push(newChannel);
+    io.emit("channelCreate", newChannel);
   });
 
   socket.on("disconnect", () => {
@@ -48,60 +79,26 @@ io.on("connection", (socket) => {
   });
 });
 
-app.post("/login", (req, res) => {
-  const { userId } = req.body;
+// app.post("/login", (req, res) => {
+//   const { userId } = req.body;
 
-  if (!onlineUserIds.includes(userId)) {
-    onlineUserIds.push(userId);
-  }
+//   if (!onlineUserIds.includes(userId)) {
+//     onlineUserIds.push(userId);
+//   }
 
-  res.status(200).send();
-});
+//   res.status(200).send();
+// });
 
-app.post("/logout", (req, res) => {
-  const { userId } = req.body;
+// app.post("/logout", (req, res) => {
+//   const { userId } = req.body;
 
-  if (onlineUserIds.includes(userId)) {
-    onlineUserIds = onlineUserIds.filter((id) => id === userId);
-  }
+//   if (onlineUserIds.includes(userId)) {
+//     onlineUserIds = onlineUserIds.filter((id) => id === userId);
+//   }
 
-  res.status(200).send();
-});
+//   res.status(200).send();
+// });
 
-app.post("/users", (req, res) => {
-  const { username } = req.body;
-
-  if (users.find((u) => u.username === username)) {
-    return res.status(400).send("user already exists");
-  }
-
-  const user = {
-    id: randomUUID(),
-    username,
-  };
-
-  users.push(user);
-
-  res.json(user);
-});
-
-app.post("/channel", (req, res) => {
-  const { channelName } = req.body;
-
-  if (channels.find((c) => c.name === channelName)) {
-    return res.status(400).json({ message: "channel already exists" });
-  }
-
-  const channel = {
-    id: randomUUID(),
-    name: channelName,
-  };
-
-  channels.push(channel);
-
-  res.json(channel);
-});
-
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
